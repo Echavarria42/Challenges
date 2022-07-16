c4f81a9f78f0a8041c7dbfec22ff34b7f176ae7e
Mateo Echavarria Sierra <63327224+TeoEchavarria@users.noreply.github.com>
2022-07-16 16:36:07 -0500

    we add a customer record and show all the records we have made

--- Basic_sql.py
+++ Basic_sql.py
@@ -8,32 +8,30 @@ my_db = mysql.connector.connect(
 )
 
 my_cursor = my_db.cursor()
-
+'''
 #create_database = "CREATE DATABASE basic_sql"
 #my_cursor.execute(create_database)               # Ejecutar 
 
 #delete_database = "DROP DATABASE basicproject02"   
 #my_cursor.execute(delete_database)
 
-"""
+
 show_databases = "SHOW DATABASES"         
 my_cursor.execute(show_databases)
 
-"""
-'''
 create_table = """ CREATE TABLE client(
     name VARCHAR (255),
     lastname VARCHAR (255)
 ) """
 my_cursor.execute(create_table)
-'''
+
 
 #delete_table = "DROP TABLE IF EXISTS client" # Elimine la tabla si existe
 #my_cursor.execute(delete_table)
 
-'''alter_table = """ ALTER TABLE clients
+alter_table = """ ALTER TABLE clients
 ADD client_id INT AUTO_INCREMENT PRIMARY KEY"""
-my_cursor.execute(alter_table)'''
+my_cursor.execute(alter_table)
 
 colums_table = "DESCRIBE clients"    
 my_cursor.execute(colums_table)
@@ -41,9 +39,19 @@ my_cursor.execute(colums_table)
 for item in my_cursor:
     print(item)
 
-"""my_cursor.execute("SHOW TABLES")
+my_cursor.execute("SHOW TABLES")
 
 for item in my_cursor:
-    print(item)"""
+    print(item)
+
+add_client = """ INSERT INTO clients
+(name, lastname)
+VALUES('Juan', 'Molina') """
+my_cursor.execute(add_client)
+my_db.commit()'''
+
+my_cursor.execute("SELECT * FROM clients")
+for client in my_cursor:
+    print(client)
 
 my_db.close() #Cerrar Base de datos
